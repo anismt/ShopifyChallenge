be981d67b219ccacccf35bb1f9bd54acc55c7ca1
Anis Tariq <anistariq@hotmail.com>
2021-01-03 16:26:38 -0500

    Made methods for each

--- colourDescrip.py
+++ colourDescrip.py
@@ -5,44 +5,65 @@ conn=sqlite3.connect('picDB.db')
 conn.text_factory = str
 cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS pic_table
-(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-name TEXT,
-data BLOP)
-""")
+
+def create_pic_table():
+   cursor.execute("""
+   CREATE TABLE IF NOT EXISTS pic_table
+   (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   name TEXT,
+   data BLOP)
+   """)
+
+def create_keyword_table():
+   cursor.execute("""
+   CREATE TABLE IF NOT EXISTS keyword_table
+   (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   keyword TEXT,
+   picture_id INT,
+   FOREIGN KEY (picture_id) REFERENCES pic_table(id))
+   """)
  
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS keyword_table
-(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-keyword TEXT,
-picture_id INT,
-FOREIGN KEY (picture_id) REFERENCES pic_table(id))
-""")
+
+
+def add_pic(name,data,keyword):
+   
+   cursor.execute("""
+   INSERT INTO pic_table (name, data) VALUES (?,?)""", (name, data))
  
+   picture_id = cursor.execute("""SELECT id FROM pic_table WHERE name=? and data=?""", [name, data]).fetchone()[0]
+
+   cursor.execute("""
+   INSERT INTO keyword_table (keyword, picture_id) VALUES (?,?)""", (keyword, picture_id))
+ 
+
+def search_table():
+   cursor.execute("""
+   SELECT pic_table.name, pic_table.data
+   FROM pic_table
+   INNER JOIN keyword_table
+   ON keyword_table.picture_id = pic_table.id
+   """)
+
+# pdb.set_trace()
+
+create_pic_table()
+create_keyword_table()
+
 
 name = 'Sunflower'
 with open('sunflower.jpg','rb') as f:
    data= f.read()
- 
-cursor.execute("""
-INSERT INTO pic_table (name, data) VALUES (?,?)""", (name, data))
- 
-keyword = 'flower'
+keyword = "flower"
 
-picture_id = cursor.execute("""SELECT id FROM pic_table WHERE name=? and data=?""", [name, data]).fetchone()[0]
-# pdb.set_trace()
-cursor.execute("""
-INSERT INTO keyword_table (keyword, picture_id) VALUES (?,?)""", (keyword, picture_id))
- 
-search_results = cursor.execute("""
-SELECT pic_table.name, pic_table.data
-FROM pic_table
-INNER JOIN keyword_table
-ON keyword_table.picture_id = pic_table.id
-""")
+add_pic(name,data,keyword)
 
-# pdb.set_trace()
+
+search_table()
+
+
+
+
+
 
 conn.commit()
 cursor.close()
