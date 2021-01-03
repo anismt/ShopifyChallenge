8ba18c63b423c0ca6192e4747febfa13bbf9a7f9
Anis Tariq <anistariq@hotmail.com>
2021-01-03 16:11:56 -0500

    built backend- need to modularize

--- colourDescrip.py
+++ colourDescrip.py
@@ -1,25 +1,49 @@
 import sqlite3
+import pdb
 
-
-conn=sqlite3.connect('image.db')
+conn=sqlite3.connect('picDB.db')
 conn.text_factory = str
 cursor = conn.cursor()
 
-m = cursor.execute("""
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS pic_table
+(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+name TEXT,
+data BLOP)
+""")
+ 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS keyword_table
+(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+keyword TEXT,
+picture_id INT,
+FOREIGN KEY (picture_id) REFERENCES pic_table(id))
+""")
+ 
 
-SELECT * FROM my_table
+name = 'Sunflower'
+with open('sunflower.jpg','rb') as f:
+   data= f.read()
+ 
+cursor.execute("""
+INSERT INTO pic_table (name, data) VALUES (?,?)""", (name, data))
+ 
+keyword = 'flower'
+
+picture_id = cursor.execute("""SELECT id FROM pic_table WHERE name=? and data=?""", [name, data]).fetchone()[0]
+# pdb.set_trace()
+cursor.execute("""
+INSERT INTO keyword_table (keyword, picture_id) VALUES (?,?)""", (keyword, picture_id))
+ 
+search_results = cursor.execute("""
+SELECT pic_table.name, pic_table.data
+FROM pic_table
+INNER JOIN keyword_table
+ON keyword_table.picture_id = pic_table.id
 """)
 
-for x in m:
-    print(x[2])
-    rec_data = x[2]
-
-with open('moon.jpg','wb') as f:
-    f.write(rec_data)
-
+# pdb.set_trace()
 
 conn.commit()
-
 cursor.close()
-
 conn.close()
