7d75a4c6253e66a7ffaa180a33b1572345dac64c
Anis Tariq <anistariq@hotmail.com>
2021-01-07 17:33:26 -0500

    Updated add_keyword

--- addSearchPics.py
+++ addSearchPics.py
@@ -52,10 +52,7 @@ def add_folder_of_pics(folder):
 
       conn.commit()
 
-
-
 def add_keyword(data,keyword):
-
 
    picture_id = cursor.execute("""SELECT id FROM pic_table WHERE data=?""", [data]).fetchone()[0]
 
@@ -76,6 +73,18 @@ def search_table(search_keyword):
 
    return pictures
 
+def pic_table_length():
+   tab_len = cursor.execute("""
+   SELECT pic_table.data
+   FROM pic_table
+   """)
+   lis = []
+
+   for x in tab_len:
+      lis.append(x[0])
+
+   conn.commit()
+   return lis
 
 def display_pictures(keyword):
    
@@ -90,17 +99,15 @@ if __name__ == "__main__":
    create_keyword_table()
    keyword_list = []
 
+
    # print("Welcome to the Picture Show! Here you will be able to add pictures, attach keywords to those pictures and then later search for those pictures.")
 
    next_pic = ""
    data = ""
    choice = ""
-   
 
    while(choice != "e"):
-
-      choice = raw_input("Type s - search, a - add picture, f - add folder, k - adding keywords or e to exit: ").lower()
-
+      choice = raw_input("Type s - search, a - add picture, f - add folder, k - adding more keywords or e to exit: ").lower()
 
       if choice == "s":
          keyword = raw_input("What is a keyword for your picture(s) (Yellow): ")
@@ -110,16 +117,22 @@ if __name__ == "__main__":
          add_pic(data)
          keyword = raw_input("Lets add a keyword to the photo " + data + ": ")
          add_keyword(data,keyword)
+         
       if choice == "k":
-         data = raw_input("What is the photo file name (e.g. sunflower.jpg): ")
-         keyword_list = list(map(str, raw_input("Put in all the keywords seperated by a space (for moon.jpg - Lune Round White) : ").split()))
-         for i in keyword_list:
-            add_keyword(data,i)
+         if len(pic_table_length()) == 0:
+            print("There are no pictures to add keywords to!")
+         else:
+            print(pic_table_length())
+            print("These are the photos currently in the database:")
+            data = raw_input("What is the photo path, select from above (e.g. sunflower.jpg, PicFolder/moon.jpg): ")
+            keyword_list = list(map(str, raw_input("Put in all the keywords seperated by a space (for moon.jpg - Lune Round White) : ").split()))         
+            for i in keyword_list:
+               add_keyword(data,i)
 
       if choice == "f":
          folder = raw_input("What is the pathway to your folder: ")
          add_folder_of_pics(folder)   
-
+         count = 1  
    print("Thank you for coming to the Picture Show! Good Day!")
 
    cursor.close()
